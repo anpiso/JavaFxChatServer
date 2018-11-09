02d22e7f3a1e7a1991aaee3b770ffee5f260a5b6
DESKTOP-2NRCQAI\DJ-Dragon <ehdwnd9238@gmail.com>
2018-11-10 06:01:24 +0900

    GUI 구현 및 주석과 코드 추가

--- src/application/Client.java
+++ src/application/Client.java
@@ -7,73 +7,81 @@ import java.net.Socket;
 
 public class Client {
 	Socket socket;
+	// 통신을 위한 소켓을 만들었다.
 
 	public Client(Socket socket) {
-		this.socket = socket;
-		receive();
+		this.socket = socket;	
+		receive();	
 	}
 
+	//클라이언트로부터 메세지를 전달받기 위한 메소드
 	public void receive() {
 		Runnable thread = new Runnable() {
 			@Override 
 			public void run() {
 				try {
 					while(true) {
-						InputStream in = socket.getInputStream();
-						byte[] buffer = new byte[512];
-						int length = in.read(buffer);
-						while(length == 1) throw new IOException();
-						System.out.println("수신 성공"
-								+ socket.getRemoteSocketAddress()
-								+ ":" 
-								+ Thread.currentThread().getName());
-						String message = new String(buffer, 0, length, "UTF-8");
+						InputStream in = socket.getInputStream();	//소켓에서 데이터를 inputStream으로 리턴을 받아서 inputStream형 in객체를 생성
+						byte[] buffer = new byte[512];				//byte형 배열인 buffer를 최대 512바이트까지 받을 수 있도록 객체 생성.
 						
-						for(Client client : Main.clients) {
-							client.send(message);
+						int length = in.read(buffer);				//inputStream으로부터 읽어들인 바이트(데이터)를 buffer에 저장하고 실제 읽어들인 바이트 수를 리턴하여 length에 저장
+						if(length == -1) 							//메세지를 읽어들일 때 오류가 발생했다면
+							throw new IOException();				//IOExeption오류를 발생시킴
+						System.out.println("수신 성공"					
+								+ socket.getRemoteSocketAddress()	//현재 접속 한 클라이언트의 정보를 출력하도록 한다.
+								+ ":" 
+								+ Thread.currentThread().getName());	//현재 스레드의 이름을 출력하게 한다.
+						String msg = new String(buffer, 0, length, "UTF-8");//buffer에서 전달받은 내용을 length만큼의 길이로 UTF-8로 인코딩하여 msg문자열을 생성
+						
+						for(Client client : Main.clients) {	//다른 클라이언트들에게 클라이언트에서 전달받은 메세지를 보여준다
+							client.send(msg);
 						}
 					}
 				}catch(Exception e) {
 					e.printStackTrace();
 					try {
 						System.out.println("메세지 수신 오류"
-								+  socket.getRemoteSocketAddress()
-								+ ":"
-								+ Thread.currentThread().getName());
-					}catch(Exception e2) {
-						e2.printStackTrace();
-
-					}
-				}
-			}
-
-		};
-		Main.threadPool.submit(thread);
-	}
-
-	public void send(String message) {
-		Runnable thread = new Runnable() {
-			@Override
-			public void run() {
-				try {
-					OutputStream out = socket.getOutputStream();
-					byte[] buffer = message.getBytes("UTF-8");
-					out.write(buffer);
-					out.flush();
-				}catch(Exception e){
-					try {
-						System.out.println("메세지 송신 오류"
-								+ socket.getRemoteSocketAddress()
+								+  socket.getRemoteSocketAddress()	
 								+ ":"
 								+ Thread.currentThread().getName());
 						Main.clients.remove(Client.this);
 						socket.close();
 					}catch(Exception e2) {
 						e2.printStackTrace();
+
+					}
+				}
+			}
+
+		};
+		Main.threadPool.submit(thread);	//Main클래스에서 만든 threadPool에 위에서 만든 thread를 넘겨서 threadPool에 추가함 
+	}
+	
+	//클라이언트에게 메세지를 전송하는 메소드
+	public void send(String msg) {
+		Runnable thread = new Runnable() {
+			@Override
+			public void run() {
+				try {
+					OutputStream out = socket.getOutputStream();	//socket에서 메세지를 내보내기 위한 OutputStream객체 생성
+					byte[] buffer = msg.getBytes("UTF-8");			//문자열 객체인 msg를 UTF-8로 인코딩하고 byte형식으로 변환하여 buffer배열 생성 
+					out.write(buffer);								//buffer에 있는 길이만큼의 바이트를 OutputStream에 쓴다.
+					out.flush();									//그리고 OutputStream에 있는 버퍼를 비워준다.
+				}catch(Exception e){
+					try {
+						System.out.println("메세지 송신 오류"
+								+ socket.getRemoteSocketAddress()
+								+ ":"
+								+ Thread.currentThread().getName());
+						Main.clients.remove(Client.this);			//오류가 발생한 클라이언트를 삭제시키고
+						socket.close();								//오류가 발생한 클라이언트에 대한 소켓을 닫아버린다.
+					}catch(Exception e2) {
+						e2.printStackTrace();
 					}
 				}
 			}
 		};
+		Main.threadPool.submit(thread);
 	}
 }
 
--- src/application/Main.java
+++ src/application/Main.java
@@ -9,73 +9,86 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
 
 
 public class Main extends Application {
 
-	public static ExecutorService threadPool;	//스레드를 제한함
+	public static ExecutorService threadPool;	//ExecutorService를 이용해서 스레드풀을 두고 스레드를 제한함
+	//스레드의 수에 제한을 두기 때문에 클라이언트가 갑자기 많아지더라도 스레드 수에 제한을 두기 때문에 성능 저하를 막을 수 있다.
+	
 	public static Vector<Client> clients = new Vector<Client>();
+	//접속한 클라이언트를 벡터 클래스를 통해 관리하도록 함
+	//Vector클래스는 일종의 배열 클래스로 ArrayList나 LinkedList처럼 사용할 수 있으며, 같은 Collection인터페이스를 상속받아 구현하고있다.
 
 	ServerSocket serverSocket;
+	
 
 	//클라이언트의 연결을 기다리는 메소드
-	public void stratServer(String IP, int port) {
+	public void startServer(String IP, int port) {
 		try {
-			serverSocket = new ServerSocket();
-			serverSocket.bind(new InetSocketAddress(IP,port));
+			serverSocket = new ServerSocket();	//서버소켓 객체를 만들고,
+			serverSocket.bind(new InetSocketAddress(IP,port));	//서버소켓에 특정 IP와 port번호를 생성하여 바인드시킨다
 
 		}catch (Exception e) {
 			e.printStackTrace();
-			if(!serverSocket.isClosed()) {
-				stopServer();
+			if(!serverSocket.isClosed()) {	//오류가 발생했을 때 서버가 닫혀있는 상태가 아니라면
+				stopServer();				//서버를 닫는다.
 			}
-			return; 
+			return;
 		}
-		//클라이언트가 접속할 때 가지 계속 기다리는 스레드
+		
+		//클라이언트가 접속할 때 까지 계속 기다리는 스레드
 		Runnable thread = new Runnable() {
 			@Override
 			public void run() {
 				while(true) {
 					try {
-						Socket socket = serverSocket.accept();
-						clients.add(new Client(socket));
+						Socket socket = serverSocket.accept();	//클라이언트의 서버 접속을 대기해서 소켓을 생성한다.
+						clients.add(new Client(socket));		//클라이언트를 Vector클래스로 만든 clients배열에 추가하여 관리하게 한다.
 						System.out.println("클라이언트 접속"
-								+ socket.getRemoteSocketAddress()
+								+ socket.getRemoteSocketAddress()	//접속한 클라이언트의 주소를 출혁
 								+ ": "
-								+ Thread.currentThread().getName());
+								+ Thread.currentThread().getName());	//해당 클라이언트의 이름을 출력
 					}catch(Exception e) {
-						if(!serverSocket.isClosed()) {
-							stopServer();
+						if(!serverSocket.isClosed()) {		//오류가 발생했을 때 서버가 닫혀있는 상태가 아니라면
+							stopServer();					//서버를 닫는다.
 						}
 						break;
 					}
 				}
 			}
 		};
-		threadPool = Executors.newCachedThreadPool();
-		threadPool.submit(thread);
+		
+		threadPool = Executors.newCachedThreadPool();	//threadPool을 초기화한 뒤
+		threadPool.submit(thread);						//threadPool에 위에서 만든 스레드를 추가시켜준다.
 
 	}
 
-	//서버 작동 준비
+	//서버 작동 중지
 	public void stopServer() {
 		try {
-			Iterator<Client> iterator = clients.iterator();
-			while(iterator.hasNext()) {
-				Client client = iterator.next();
-				client.socket.close();
-				iterator.remove();
+			Iterator<Client> iterator = clients.iterator();	//Vector클래스로 만든 clients에 접근하기 위한 iterator를 생성
+			while(iterator.hasNext()) {						//하나하나의 클라이언트에 접근하여
+				Client client = iterator.next();			
+				client.socket.close();						//클라이언트의 소켓을 닫아버리고
+				iterator.remove();							//iterator에서 지워버린다.
 			}
+			
 			//서버 소켓 객체 닫기
-			if(serverSocket != null && !serverSocket.isClosed()) {
-				serverSocket.close();
+			if(serverSocket != null && !serverSocket.isClosed()) {	//서버소켓이 null값이 아니고 서버소켓이 열려있는 상태라면 
+				serverSocket.close();								//서버소켓을 닫는다.
 			}
 			//스레드풀 종료
-			if(threadPool != null && !serverSocket.isClosed()) {
-				threadPool.shutdown();
+			if(threadPool != null && !serverSocket.isClosed()) {	//스레드풀에 어떠한 스레드도 없고 서버소켓이 열려있는 상태라면
+				threadPool.shutdown();								//스레드풀에서 대기중인 작업을 모두 중지하고 스레드풀을 반납한다.
 			}
 		}catch(Exception e) {
 			e.printStackTrace();
@@ -85,7 +98,46 @@ public class Main extends Application {
 	//UI생성하고 프로그램 동작시키는 메소드
 	@Override
 	public void start(Stage primaryStage) {
+		BorderPane root = new BorderPane();
+		root.setPadding(new Insets(5));
 		
+		TextArea textArea = new TextArea();
+		textArea.setEditable(false);
+		textArea.setFont(new Font("굴림", 15));
+		root.setCenter(textArea);
+		
+		Button toggleButton = new Button("시작하기");
+		toggleButton.setMaxWidth(Double.MAX_VALUE);
+		BorderPane.setMargin(toggleButton, new Insets(1, 0, 0, 0));
+		root.setBottom(toggleButton);
+		
+		String IP = "127.0.0.1";	//localhost주소 
+		int port = 8008;
+		
+		toggleButton.setOnAction(event -> {
+			if(toggleButton.getText().equals("시작하기")) {
+				startServer(IP, port);
+				Platform.runLater(() -> {
+					String msg = String.format("[서버 시작]\n", IP, port);
+					textArea.appendText(msg);
+					toggleButton.setText("종료");
+				});
+			} else {
+				stopServer();
+				Platform.runLater(() -> {
+					String msg = String.format("[서버 종료]\n", IP, port);
+					textArea.appendText(msg);
+					toggleButton.setText("시작");
+				});
+			}
+		});
+		
+		
+		Scene scene = new Scene(root, 400, 400);
+		primaryStage.setTitle("채팅 서버");
+		primaryStage.setOnCloseRequest(event -> stopServer());
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 
 
